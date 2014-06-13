d70f3ca4a762740a0f6404ae88d1eb37af25bcb1
susu99 <user.smy@gmail.com>
2014-06-13 11:08:14 +0630

    Update Person.java
    Add "id" properties.

--- src/pojo/Person.java
+++ src/pojo/Person.java
@@ -2,8 +2,19 @@ package pojo;
 
 public class Person {
 
+	private String id;
 	private String name;
 
+	public String getId() {
+		
+		return this.id;
+	}
+	
+	public void setId(String id) {
+		
+		this.id = id;
+	}
+	
 	public String getName() {
 		return name;
 	}
